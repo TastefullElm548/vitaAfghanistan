918d9eda585200a0b759258c6fb937f1a358cee4
Maso <tastefullelm548@outlook.it>
2022-04-03 20:17:47 +0200

    Aggiunto Commento ad app.js e rimosso desktop.ini

--- risorse/app.js
+++ risorse/app.js
@@ -1,8 +1,8 @@
-var messaggioDownload = "Grazie per aver scaricato la versione originale de 'La Vita in Afghanistan'";
-var nuovoPulsanteDownload = "Scaricalo di nuovo"
-
+// Download Versione Originale
 function download(){
     window.open("https://masos.my.to/vita-afghanistan/download");
+    var messaggioDownload = "Grazie per aver scaricato la versione originale de 'La Vita in Afghanistan'";
+    var nuovoPulsanteDownload = "Scaricalo di nuovo"
     document.getElementById("descrizione").textContent = messaggioDownload;
     document.getElementById("pulsanteDownload").textContent = nuovoPulsanteDownload;
 }
