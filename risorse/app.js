ea5c0840cefa908b6e9eb887afdec2516012c1a5
Maso <tastefullelm548@outlook.it>
2022-04-06 17:39:53 +0200

    Rimosso play.html e spostata home del gioco in index.html

--- risorse/app.js
+++ risorse/app.js
@@ -1,8 +1,35 @@
 // Download Versione Originale
-function download(){
+function download() {
     window.open("https://masos.my.to/vita-afghanistan/download");
     var messaggioDownload = "Grazie per aver scaricato la versione originale de 'La Vita in Afghanistan'";
     var nuovoPulsanteDownload = "Scaricalo di nuovo"
     document.getElementById("descrizione").textContent = messaggioDownload;
-    document.getElementById("pulsanteDownload").textContent = nuovoPulsanteDownload;
+    document.getElementById("primoPulsante").textContent = nuovoPulsanteDownload;
+}
+// Cambio schermata -> da homepage a gioco vero e proprio
+function launchGame() {
+    var pulsanteGioca = "Gioca";
+    var pulsanteInformazioni = "Informazioni";
+    document.getElementById("secondoPulsante").textContent = pulsanteGioca;
+    document.getElementById("secondoPulsante").setAttribute("onclick", "sceltaPersonaggio()");
+    document.getElementById("primoPulsante").textContent = pulsanteInformazioni;
+    document.getElementById("primoPulsante").setAttribute("onclick", "apriInformazioni()");
+}
+
+// Cambio schermata --> da inizio gioco a selezione personaggio
+function sceltaPersonaggio() {
+    window.location.href='play/Game/select.html';
+}
+
+// Cambio schermata --> schermate secondarie
+function apri(pagina) {
+    if (pagina === "informazioni") {
+        window.location.href='play/info.html';
+    } else {alert("Errore");
+};
+}
+
+// Workaround
+function apriInformazioni() {
+    apri("informazioni")
 }
