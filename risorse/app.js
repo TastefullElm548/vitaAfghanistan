eebadad3755b0385eb884b1cb9d5fbc1f2b0159c
Maso <tastefullelm548@outlook.it>
2022-04-07 19:30:10 +0200

    Inserito info.html in index.html

--- risorse/app.js
+++ risorse/app.js
@@ -1,8 +1,12 @@
+// Modifica Pagina
+function modificaContenuto(elemento, contenuto) {
+    document.getElementById(elemento).innerText = contenuto;
+}
+
 // Download Versione Originale
 function downloadOriginale() {
     window.open("https://masos.my.to/vita-afghanistan/download");
-    var messaggioDownload = "Grazie per aver scaricato la versione originale de 'La Vita in Afghanistan'. Se il download non parte cliccare nuovamente su Originale.";
-    document.getElementById("descrizione").textContent = messaggioDownload;
+    modificaContenuto("descrizione", "Grazie per aver scaricato la versione originale de 'La Vita in Afghanistan'. Se il download non parte cliccare nuovamente su Originale.")
 }
 // Apertura scelta personaggio
 function gioca() {
@@ -11,5 +15,36 @@ function gioca() {
 
 // Apertura informazioni
 function apriInformazioni() {
-        window.location.href='play/info.html';
+    modificaContenuto("descrizione", "Come Giocare: \nPremere un'azione per scegliere il proprio futuro \nModalità di Gioco: \nModalità Donna: le scelte contro i talebani e la Sharia fanno perdere punti vita. Se si arriva a 0 si perde. Si inizia con 45 punti vita \nModalità Uomo: le scelte contro i talebani e la Sharia riducono il livello di fiducia nei nostri confronti. Se si raggiunge 0 si viene arrestati e si perde.");
+    document.getElementById("primoPulsante").classList.add("pulsantePrincipale")
+    document.getElementById("terzoPulsante").classList.remove("pulsantePrincipale")
+    modificaContenuto("primoPulsante", "Indietro");
+    modificaContenuto("secondoPulsante", "Fonti");
+    modificaContenuto("terzoPulsante", "Crediti");
+    document.getElementById("primoPulsante").setAttribute("onclick", "indietro()");
+    document.getElementById("secondoPulsante").setAttribute("onclick", "apriFonti()");
+    document.getElementById("terzoPulsante").setAttribute("onclick", "apriCrediti()");
+    var aCapo = document.createElement("br");
+    document.getElementById("descrizione").appendChild(aCapo);
+    var linkInformazioni = document.createElement("a");
+    var contenutoLinkInformazioni = document.createTextNode("Perché 45?");
+    linkInformazioni.appendChild(contenutoLinkInformazioni);
+    document.getElementById("descrizione").appendChild(linkInformazioni);
+    linkInformazioni.href = "https://oceanhero.today/it/web?q=Vita+media+Afghanistan";
+    linkInformazioni.target = "_blank";
+}
+
+// Tornare indietro
+function indietro() {
+    window.location.reload(false);
+}
+
+// Aprire fonti
+function apriFonti() {
+    window.location.href='play/fonti.html'
+}
+
+// Aprire crediti
+function apriCrediti() {
+    window.location.href='play/crediti.html'
 }
