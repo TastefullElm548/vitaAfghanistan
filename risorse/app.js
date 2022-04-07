1945f930effe7c424e94954f42985e0b83701619
Maso <tastefullelm548@outlook.it>
2022-04-07 18:41:39 +0200

    Migliorata schermata home

--- risorse/app.js
+++ risorse/app.js
@@ -1,35 +1,15 @@
 // Download Versione Originale
-function download() {
+function downloadOriginale() {
     window.open("https://masos.my.to/vita-afghanistan/download");
-    var messaggioDownload = "Grazie per aver scaricato la versione originale de 'La Vita in Afghanistan'";
-    var nuovoPulsanteDownload = "Scaricalo di nuovo"
+    var messaggioDownload = "Grazie per aver scaricato la versione originale de 'La Vita in Afghanistan'. Se il download non parte cliccare nuovamente su Originale.";
     document.getElementById("descrizione").textContent = messaggioDownload;
-    document.getElementById("primoPulsante").textContent = nuovoPulsanteDownload;
 }
-// Cambio schermata -> da homepage a gioco vero e proprio
-function launchGame() {
-    var pulsanteGioca = "Gioca";
-    var pulsanteInformazioni = "Informazioni";
-    document.getElementById("secondoPulsante").textContent = pulsanteGioca;
-    document.getElementById("secondoPulsante").setAttribute("onclick", "sceltaPersonaggio()");
-    document.getElementById("primoPulsante").textContent = pulsanteInformazioni;
-    document.getElementById("primoPulsante").setAttribute("onclick", "apriInformazioni()");
-}
-
-// Cambio schermata --> da inizio gioco a selezione personaggio
-function sceltaPersonaggio() {
+// Apertura scelta personaggio
+function gioca() {
     window.location.href='play/Game/select.html';
 }
 
-// Cambio schermata --> schermate secondarie
-function apri(pagina) {
-    if (pagina === "informazioni") {
-        window.location.href='play/info.html';
-    } else {alert("Errore");
-};
-}
-
-// Workaround
+// Apertura informazioni
 function apriInformazioni() {
-    apri("informazioni")
+        window.location.href='play/info.html';
 }
