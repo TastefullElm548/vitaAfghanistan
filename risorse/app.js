db53dea0e92ea7b50f3a5a96ff9e5c7636fb557a
Maso <tastefullelm548@outlook.it>
2022-04-08 19:06:40 +0200

    Splitted index.html in index.html, about, select and play

--- risorse/app.js
+++ risorse/app.js
@@ -15,36 +15,20 @@ function gioca() {
 
 // Apertura informazioni
 function apriInformazioni() {
-    modificaContenuto("descrizione", "Come Giocare: \nPremere un'azione per scegliere il proprio futuro \nModalità di Gioco: \nModalità Donna: le scelte contro i talebani e la Sharia fanno perdere punti vita. Se si arriva a 0 si perde. Si inizia con 45 punti vita \nModalità Uomo: le scelte contro i talebani e la Sharia riducono il livello di fiducia nei nostri confronti. Se si raggiunge 0 si viene arrestati e si perde.");
-    document.getElementById("primoPulsante").classList.add("pulsantePrincipale")
-    document.getElementById("terzoPulsante").classList.remove("pulsantePrincipale")
-    modificaContenuto("primoPulsante", "Indietro");
-    modificaContenuto("secondoPulsante", "Fonti");
-    modificaContenuto("terzoPulsante", "Crediti");
-    document.getElementById("primoPulsante").setAttribute("onclick", "indietro()");
-    document.getElementById("secondoPulsante").setAttribute("onclick", "apriFonti()");
-    document.getElementById("terzoPulsante").setAttribute("onclick", "apriCrediti()");
-    var aCapo = document.createElement("br");
-    document.getElementById("descrizione").appendChild(aCapo);
-    var linkInformazioni = document.createElement("a");
-    var contenutoLinkInformazioni = document.createTextNode("Perché 45?");
-    linkInformazioni.appendChild(contenutoLinkInformazioni);
-    document.getElementById("descrizione").appendChild(linkInformazioni);
-    linkInformazioni.href = "https://oceanhero.today/it/web?q=Vita+media+Afghanistan";
-    linkInformazioni.target = "_blank";
+    window.location.href='about';
 }
 
 // Tornare indietro
-function indietro() {
-    window.location.reload(false);
+function indietro(destinazione) {
+    window.location.href=destinazione;
 }
 
 // Aprire fonti
 function apriFonti() {
-    window.location.href='play/fonti.html'
+    window.location.href='GiocoLegacy/fonti.html'
 }
 
 // Aprire crediti
 function apriCrediti() {
-    window.location.href='play/crediti.html'
+    window.location.href='GiocoLegacy/crediti.html'
 }
