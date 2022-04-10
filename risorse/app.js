d9886d43f5521b2de419603b4b3d6b8d5edeeb4c
Maso <tastefullelm548@outlook.it>
2022-04-10 12:05:52 +0200

    Ridotto JS

--- risorse/app.js
+++ risorse/app.js
@@ -3,32 +3,13 @@ function modificaContenuto(elemento, contenuto) {
     document.getElementById(elemento).innerText = contenuto;
 }
 
+// Link ad altre pagine interne
+function vai(destinazione) {
+    window.location.replace(destinazione);
+}
+
 // Download Versione Originale
 function downloadOriginale() {
     window.open("https://masos.my.to/vita-afghanistan/download");
     modificaContenuto("descrizione", "Grazie per aver scaricato la versione originale de 'La Vita in Afghanistan'. Se il download non parte cliccare nuovamente su Originale.")
-}
-// Apertura scelta personaggio
-function gioca() {
-    window.location.href='play';
-}
-
-// Apertura informazioni
-function apriInformazioni() {
-    window.location.href='about';
-}
-
-// Tornare indietro
-function indietro(destinazione) {
-    window.location.href=destinazione;
-}
-
-// Aprire fonti
-function apriFonti() {
-    window.location.href='../GiocoLegacy/fonti.html'
-}
-
-// Aprire crediti
-function apriCrediti() {
-    window.location.href='../GiocoLegacy/crediti.html'
 }
