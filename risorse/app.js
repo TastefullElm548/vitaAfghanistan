106ba338ad05ad199dcb48037d9b3b7b82c564c3
Maso <tastefullelm548@outlook.it>
2022-04-11 19:09:31 +0200

    Aggiornata Selezione Genere in app.js

--- risorse/app.js
+++ risorse/app.js
@@ -11,5 +11,18 @@ function vai(destinazione) {
 // Download Versione Originale
 function downloadOriginale() {
     window.open("https://masos.my.to/vita-afghanistan/download");
-    modificaContenuto("descrizione", "Grazie per aver scaricato la versione originale de 'La Vita in Afghanistan'. Se il download non parte cliccare nuovamente su Originale.")
+    modificaContenuto("descrizione", "Grazie per aver scaricato la versione originale de 'La Vita in Afghanistan'. Se il download non parte cliccare nuovamente su Originale.");
+}
+
+// Selezione Personaggio
+function seleziona(genere) {
+    if (genere === "donna") {
+        if (confirm("Vuoi Continuare come Donna? \n \n Le scelte ti riguarderanno personalmente, rivendica i tuoi diritti riuscendo a sopravvivere \n \n Livello di difficoltà: Difficile") == true) {
+            vai('../play/donna/donna.html');
+        }
+    }   else if (genere === "uomo") {
+        if (confirm("Vuoi Continuare come Uomo? \n \n Le scelte riguarderanno il tuo pensiero nei confronti dei diritti delle donne e dei talebani, fai la cosa giusta riuscendo a sopravvivere \n \n Livello di difficoltà: Medio") == true) {
+            vai('../play/uomo/uomo.html');
+        }
+    }
 }
