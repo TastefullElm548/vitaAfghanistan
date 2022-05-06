759da06b03d977281fd43b806dd5748e183e35ea
Maso <tastefullelm548@outlook.it>
2022-05-06 18:04:09 +0200

    Sistemate conseguenzePulsante2

--- risorse/uPoints.js
+++ risorse/uPoints.js
@@ -13,7 +13,7 @@ const testoPulsante2 = ["Si, ne parlerò con i miei genitori", "Ne parlo con i m
 const testoPulsante3 = ["Si, ne parlerò a scuola", "Protesto per far si che non siano obbligate", "Dovrei chiedere il loro parere, no?", "Cerco libri in giro per i mercati", "Andando in giro non curanti delle leggi", "Li crescero senza mandarli alle scuole \"talebane\"", "Abolizione del dominio talebano e liberazione dell'Afghanistan"];
 
 const conseguenzePulsante1 = ["Conseguenze: \n \nEssere troppo ingenui, a volte, fa accorgere delle cose quando è troppo tardi", "Conseguenze: \n \nSperiamo che ti lascino scegliere al 100%", "Conseguenze: \n \nFamiglia Imparentata: \nOgni tuo desiderio è un ordine per noi", "Conseguenze: \n \nL'ignoranza semplifica la vita, ma non ti porterà mai alla realizzazione e alla felicità vera", "Conseguenze: \n \nNon è mai troppo tardi per il cambiamento! (cit. AWorld)", "Conseguenze: \n \nÈ probabile che seguiranno i tuoi esempi nonostante ciò!", "Conseguenze: \n \nHai concluso la tua vita in modo dignitoso. Sei morto da eroe! I talebani ti vedono come un nemico della patria e cercano di ucciderti, fuggi all'estero verso l'Europa e speri che i tuoi figli, quando non ci sarai più, riporteranno l'Afghanistan al suo antico splendore! Addio Combattente!"];
-const conseguenzePulsante2 = [];
+const conseguenzePulsante2 = ["Conseguenze: \n \nTi spiegano la situazione e ti sembrano un po' preoccupati per te", "Conseguenze: \n \nTi spiegano che le donne sono solo oggetti di scambio con le altre famiglie e anche tu ne riceverai una. Ciò non ti sembra giusto", "Conseguenze: \n \nLa tua famiglia: \nVabbé, abbiamo tanti cugini e tanti amici ancora...", "Conseguenze: \n \nGli altri non capiscono questo tuo desiderio e ti scherniscono", "Conseguenze: \n \nLei si sta rilassando perché pensa di poter vivere un futuro migliore!", "Conseguenze: \n \nQuesti sono la parte più importante dell'educazione dei figli. Continua così!", "Conseguenze: \n \nHai concluso la tua vita in modo dignitoso. Sei morto da eroe! I talebani ti vedono come un nemico della patria e cercano di ucciderti, fuggi all'estero verso l'Europa e speri che i tuoi figli, quando non ci sarai più, riporteranno l'Afghanistan al suo antico splendore! Addio Combattente!"];
 const conseguenzePulsante3 = [];
 
 function cambiaConseguenze(numeroPulsante, reputazioneDaPerdere) {
@@ -33,14 +33,6 @@ function apriSpiegazione(numeroPulsante, numeroDomanda) {
 
 var counterArrays = 0;
 var nBtnVar = "";
-
-var b0 = "Conseguenze: \n \nTi spiegano la situazione e ti sembrano un po' preoccupati per te";
-var b1 = "Conseguenze: \n \nTi spiegano che le donne sono solo oggetti di scambio con le altre famiglie e anche tu ne riceverai una. Ciò non ti sembra giusto";
-var b2 = "Conseguenze: \n \nLa tua famiglia: \nVabbé, abbiamo tanti cugini e tanti amici ancora...";
-var b3 = "Conseguenze: \n \nGli altri non capiscono questo tuo desiderio e ti scherniscono";
-var b4 = "Conseguenze: \n \nLei si sta rilassando perché pensa di poter vivere un futuro migliore!";
-var b5 = "Conseguenze: \n \nQuesti sono la parte più importante dell'educazione dei figli. Continua così!";
-var b6 = "Conseguenze: \n \nHai concluso la tua vita in modo dignitoso. Sei morto da eroe! I talebani ti vedono come un nemico della patria e cercano di ucciderti, fuggi all'estero verso l'Europa e speri che i tuoi figli, quando non ci sarai più, riporteranno l'Afghanistan al suo antico splendore! Addio Combattente!";
 
 var c0 = "Conseguenze: \n \nTi urlano di non lasciare che pensieri incostituzionali ti entrino in mente e iniziano a guardarti male";
 var c1 = "Conseguenze: \n \nI tuoi genitori ti picchiano perché non devi opporti alla legge!";
